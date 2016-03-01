9debb315e248921fa68e9943d6b2544bd26090a3
Bao Tran <btran.it@gmail.com>
2016-03-01 15:13:39 -0800

    Move script from index.html to its own callscript.js file
    This is to keep JavaScript separated from HTML file.

---
+++ js/callscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("#lightgallery").lightGallery({
+	loadYoutubeThumbnail: true,
+	youtubeThumbSize: 'default',
+	loadVimeoThumbnail: true,
+	vimeoThumbSize: 'thumbnail_medium'
+	}); 
+
+});
